d342d778d23dd89fb0e94451f0b2a1f0a05cf19e
Luong Le <luong@originalstitch.com>
2022-11-27 08:48:41 +0700

    Fix constants

--- fixtures/constants.js
+++ fixtures/constants.js
@@ -1,2 +1,2 @@
 export const THETA_EXPLORER_URL = 'https://explorer.thetatoken.org'
-export const THETAPIZZA_API_ENDPOINT = 'http://localhost:3001/v1'; //'https://api.thetapizza.com/v1'
+export const THETAPIZZA_API_ENDPOINT = 'https://api.thetapizza.com/v1'
